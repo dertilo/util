c0ade7cb36120f59b75049d89d7fab3e9e284074
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2019-10-08 22:38:16 +0200

    skip lines in read_jsonl; no try-except in write_jsonl

--- util/data_io.py
+++ util/data_io.py
@@ -5,20 +5,16 @@ import os
 
 def write_jsonl(file:str,data:Iterable[Dict], mode="b"):
     def process_line(d:Dict):
-        try:
-            line = json.dumps(d, skipkeys=True, ensure_ascii=False)
-            line = line + "\n"
-            if mode=='b':
-                line = line.encode('utf-8')
-        except Exception as e:
-            line=''
-            print(e)
+        line = json.dumps(d, skipkeys=True, ensure_ascii=False)
+        line = line + "\n"
+        if mode=='b':
+            line = line.encode('utf-8')
         return line
     with gzip.open(file, mode='w'+mode) if file.endswith('gz') else open(file, mode='w'+mode) as f:
         f.writelines((process_line(d) for d in data))
 
-def write_json(file:str,datum:Dict,mode="wb"):
-    with gzip.open(file, mode=mode) if file.endswith('gz') else open(file, mode=mode) as f:
+def write_json(file:str,datum:Dict,mode="b"):
+    with gzip.open(file, mode='w'+mode) if file.endswith('gz') else open(file, mode='w'+mode) as f:
         line = json.dumps(datum,skipkeys=True, ensure_ascii=False)
         line = line.encode('utf-8')
         f.write(line)
@@ -50,13 +46,12 @@ def read_lines(file, mode ='b', encoding ='utf-8', limit=None):
                 line = line.decode(encoding)
             yield line.replace('\n','')
 
-def read_jsonl(file, mode="b",limit=None):
+def read_jsonl(file, mode="b",limit=None,num_to_skip=0):
     assert any([mode==m for m in ['b','t']])
     with gzip.open(file, mode='r'+mode) if file.endswith('.gz') else open(file, mode="rb") as f:
-        counter=0
-        for line in f:
-            counter += 1
-            if limit and (counter > limit): break
+        [next(f) for _ in range(num_to_skip)]
+        for line,k in enumerate(f):
+            if limit and (k > limit): break
             yield json.loads(line.decode('utf-8') if mode=='b' else line)
 
 def read_json(file:str,mode="b"):
