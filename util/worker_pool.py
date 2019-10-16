651bff94bc74e2a6cf3c656e39cb0d970a0a9f04
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2019-10-16 10:32:05 +0200

    Task-class in worker_pool

--- util/worker_pool.py
+++ util/worker_pool.py
@@ -1,64 +1,72 @@
 import time
 import traceback
-from functools import partial
+from abc import abstractmethod
 from pprint import pprint
 from typing import Iterable
-
 from torch import multiprocessing
+multiprocessing.set_start_method('spawn', force=True)  # needs to be done to get CUDA working
+# multiprocessing = None
 
+class Task(object):
+    # def __init__(self):
+        # global multiprocessing #TODO: the Task cannot influence the Worker
+        # if multiprocessing is None:
+        #     if i_want_to_use_torch_cuda:
+        #         from torch import multiprocessing
+        #         multiprocessing.set_start_method('spawn', force=True)  # needs to be done to get CUDA working
+        #     else:
+        #         import multiprocessing
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    @abstractmethod
+    def __call__(self, data):
+        raise NotImplementedError
 
 class Worker(multiprocessing.Process):
 
-    def __init__(self, task_queue, result_queue,task_fun , task_fun_kwargs_supplier):
+    def __init__(self, task_queue, result_queue, task:Task):
         multiprocessing.Process.__init__(self)
         self.task_queue = task_queue
         self.result_queue = result_queue
-        self.task_fun = task_fun
-        self.task_fun_kwargs_supplier = task_fun_kwargs_supplier
-
+        self.task:Task = task
 
     def run(self):
         proc_name = self.name
+        with self.task as task:
+            while True:
+                task_data = self.task_queue.get()
+                if task_data is None:
+                    # Poison pill means shutdown
+                    self.task_queue.task_done()
+                    break
+                try:
+                    result = task(task_data)
+                except Exception as e:
+                    traceback.print_exc()
+                    result = None
 
-        def partial_fun_builder(task_fun, task_fun_kwargs_supplier):
-            kwargs = task_fun_kwargs_supplier()
-
-            def fun(datum):
-                return task_fun(datum, **kwargs)
-
-            return fun
-
-        partial_fun = partial_fun_builder(self.task_fun,self.task_fun_kwargs_supplier)
-        while True:
-            task_data = self.task_queue.get()
-            if task_data is None:
-                # Poison pill means shutdown
-                # print('%s: Exiting' % proc_name)
                 self.task_queue.task_done()
-                break
-            try:
-                result = partial_fun(task_data)
-            except Exception as e:
-                traceback.print_exc()
-                result = None
-
-            self.task_queue.task_done()
-            self.result_queue.put(result)
+                self.result_queue.put(result)
 
 class WorkerPool(object):
 
-    def __init__(self, processes, task_fun, task_fun_kwargs_supplier, daemons=True) -> None:
+    def __init__(self, processes,task:Task, daemons=True) -> None:
         super().__init__()
         self.n_jobs = processes
         self.task_queue = multiprocessing.JoinableQueue()
         self.results_queue = multiprocessing.Queue()
-        self.task_fun = task_fun
-        self.task_fun_kwargs_supplier = task_fun_kwargs_supplier
+        self.task = task
         self.daemons = daemons
 
     def __enter__(self):
-        consumers = [Worker(self.task_queue, self.results_queue,self.task_fun,self.task_fun_kwargs_supplier) for i in range(self.n_jobs)]
+        consumers = [Worker(self.task_queue,
+                            self.results_queue,
+                            self.task) for i in range(self.n_jobs)]
         for w in consumers:
             w.daemon = self.daemons
             w.start()
@@ -86,22 +94,29 @@ def funfun(x):
     s = 'non-daemon-process: %s: %s' % (str(multiprocessing.current_process()), x)
     return s
 
+class MinimalTask(Task):
 
-def minimal_task_kwargs_supplier():
-    worker_name = str(multiprocessing.current_process())
-    kwargs = {'some_serializable_arg': 'some_arg','worker_name':worker_name}
-    print('supplying kwargs "%s" for worker: %s'%(str(kwargs),worker_name))
-    return kwargs
+    def __init__(self,params_that_where_pickled) -> None:
+        super().__init__()
+        self.params_that_where_pickled = params_that_where_pickled
 
-def minimal_task_fun(datum,some_serializable_arg,worker_name):
-    with multiprocessing.Pool(processes=3) as p:
-        result = list(p.imap_unordered(funfun,[datum+'-%s-subtask-%d'%(some_serializable_arg,k) for k in range(9)]))
-    return {'worker-name':worker_name,'results':result}
+    def __enter__(self):
+        self.variable_that_is_never_pickled = str(multiprocessing.current_process())
+        return self
+
+    def __call__(self, data):
+        with multiprocessing.Pool(processes=3) as p:
+            result = list(
+                p.imap_unordered(funfun, [data + '-%s-subtask-%d' % (self.params_that_where_pickled, k) for k in range(9)]))
+        return {'worker-name': self.variable_that_is_never_pickled, 'results': result}
+
 
 if __name__ == '__main__':
 
     data = ['task-%d'%x for x in range(3)]
-    with WorkerPool(processes=2, task_fun=minimal_task_fun, task_fun_kwargs_supplier=minimal_task_kwargs_supplier, daemons=False) as p:
+    with WorkerPool(processes=2,
+                    task = MinimalTask('some-param'),
+                    daemons=False) as p:
         x = list(p.process_unordered(data))
     print(len(x))
     pprint(x)
